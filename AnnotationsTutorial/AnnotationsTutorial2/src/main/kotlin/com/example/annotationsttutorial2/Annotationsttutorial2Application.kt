f4a738dc5cf70516cbc037079d1f13c9f4ea4faa
amrbakri <amrmb09@yahoo.com>
2020-03-24 18:01:08 +0100

    Add files via upload

---
+++ AnnotationsTutorial/AnnotationsTutorial2/src/main/kotlin/com/example/annotationsttutorial2/Annotationsttutorial2Application.kt
@@ -0,0 +1,12 @@
+package com.example.annotationsttutorial2
+
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Annotationsttutorial2Application
+
+fun main(args: Array<String>) {
+    runApplication<Annotationsttutorial2Application>(*args)
+}
